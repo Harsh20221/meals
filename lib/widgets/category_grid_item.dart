5f9be53ee899cacdab9b8f108d3c8d33ba3201a7
Harsh Kumar <harsh20221@gmail.com>
2024-05-08 06:14:47 +0530

    category grid

---
+++ lib/widgets/category_grid_item.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+class CategoryGridItem extends StatelessWidget{
+  const CategoryGridItem ({required this.category});
+  final Category category ; 
+
+  @override ///! Make sure to write override's starting letter in small not capital 
+  Widget build(BuildContext context){
+    return Container(decoration: BoxDecoration(gradient: LinearGradient(colors: [category.color.withOpacity(0.55),category.color.withOpacity(0.9)],begin: Alignment.topLeft,end: Alignment.bottomRight)),);
+  }
+}
