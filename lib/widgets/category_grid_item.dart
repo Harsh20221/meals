87606cd5a2c55af0b34d8e421b0ca024436af8f6
Harsh Kumar <harsh20221@gmail.com>
2024-05-08 06:30:51 +0530

    Text

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
-class CategoryGridItem extends StatelessWidget{
-  const CategoryGridItem ({required this.category});
-  final Category category ; 
 
-  @override ///! Make sure to write override's starting letter in small not capital 
-  Widget build(BuildContext context){
-    return Container(decoration: BoxDecoration(gradient: LinearGradient(colors: [category.color.withOpacity(0.55),category.color.withOpacity(0.9)],begin: Alignment.topLeft,end: Alignment.bottomRight)),);
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({required this.category});
+  final Category category;
+
+  @override
+
+  ///! Make sure to write override's starting letter in small not capital
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+        category.color.withOpacity(0.55),
+        category.color.withOpacity(0.9)
+      ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+      child: Text(category.title,
+          style: Theme.of(context)
+              .textTheme
+              .titleLarge!
+              .copyWith(color: Theme.of(context).colorScheme.onBackground)),
+    );
   }
 }
