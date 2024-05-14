0e7304e75fe56fd46c37b86c61738d60a67465ac
Harsh Kumar <harsh20221@gmail.com>
2024-05-14 23:20:41 +0530

    more changes

--- lib/widgets/meal_item_trait.dart
+++ lib/widgets/meal_item_trait.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 ////import 'package:meals/widgets/meal_item_trait.dart';
 //////import 'package:meals/widgets/meal_item.dart';
-//* This file will be used to create the meal item trait widget that will be used to display the meal item trait in the meal screen this'll store the metadata of the mealmeans it'll store the duration,complexity and the affordability of the meal
+//* This file will be used to create the meal item trait widget that will be used to display the meal item trait in the meal screen this'll store the metadata of the meal items or we can say this is just a widget called as mealitem that contains a row of icon and text 
 class MealItemTrait extends StatelessWidget {
   const MealItemTrait({super.key,required this.icon,required this.label});
   final IconData icon;
