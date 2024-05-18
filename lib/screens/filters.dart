469fe6fd0de74e51d2023059133385254dec89b9
Harsh Kumar <harsh20221@gmail.com>
2024-05-18 05:53:43 +0530

    Fixed boolscope Bugs

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
  //* This file contains the filter screen 
+ enum Filter{
+  glutenFree,
+  lactoseFree,
+  vegetarian,
+  vegan
+ }
 class FiltersScreen extends StatefulWidget {
   const FiltersScreen({super.key});
   @override
@@ -21,66 +27,75 @@ class _FiltersScreenState extends State<FiltersScreen> {
       appBar: AppBar(
         title: const Text("Filters"),
       ),
-      body: Column(
-        children: [
-          SwitchListTile(
-            value: _gluttenFreeFilterset,
-            onChanged: (ischecked) {setState(() {
-              _gluttenFreeFilterset=ischecked;//todo: Make sure to assign is checked to _gluttenfreeFilterSet in order to check if the Toggle is on or Off 
-            });},  //? is Checked will check if the ToggleButton is pressed on or off 
-            title: Text(
-              "Gluten Free",
-              style: Theme.of(context)
-                  .textTheme
-                  .titleLarge!
-                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
-            ),
-            subtitle: const Text("Only includes Gluten-Free Meals "),
-            activeColor: Theme.of(context).colorScheme.tertiary,
-          )
-        ,  SwitchListTile(
-            value: _lactoseFreeFilterset,
-            onChanged: (ischecked) {setState(() {
-              _lactoseFreeFilterset=ischecked;
-            });}, 
-            title: Text(
-              "Lactose Free ",
-              style: Theme.of(context)
-                  .textTheme
-                  .titleLarge!
-                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
-            ),
-            subtitle: const Text("Only includes Lactose-Free Meals "),
-            activeColor: Theme.of(context).colorScheme.tertiary,
-          ),  SwitchListTile(
-            value: vegatarianFilterset,
-            onChanged: (ischecked) {setState(() {
-              vegatarianFilterset=ischecked;
-            });}, 
-            title: Text(
-              "Vegetarian",
-              style: Theme.of(context)
-                  .textTheme
-                  .titleLarge!
-                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
-            ),
-            subtitle: const Text("Only includes Vegetarian Meals "),
-            activeColor: Theme.of(context).colorScheme.tertiary,
-           ), SwitchListTile(
-            value: _veganFilterset,
-            onChanged: (ischecked) {setState(() {
-              _veganFilterset=ischecked;
-            });}, 
-            title: Text(
-              "Vegan",
-              style: Theme.of(context)
-                  .textTheme
-                  .titleLarge!
-                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
-            ),
-            subtitle: const Text("Only includes Vegan  Meals "),
-            activeColor: Theme.of(context).colorScheme.tertiary,
-          )],
+      body: PopScope( canPop: false,onPopInvoked: (bool didPop){
+        if(didPop )return; Navigator.of(context).pop({
+          Filter.glutenFree:_gluttenFreeFilterset,
+         Filter.lactoseFree: _lactoseFreeFilterset,
+         Filter.vegetarian:vegatarianFilterset,
+         Filter.vegan:_veganFilterset,
+        });
+      },
+        child: Column(
+          children: [
+            SwitchListTile(
+              value: _gluttenFreeFilterset,
+              onChanged: (ischecked) {setState(() {
+                _gluttenFreeFilterset=ischecked;//todo: Make sure to assign is checked to _gluttenfreeFilterSet in order to check if the Toggle is on or Off 
+              });},  //? is Checked will check if the ToggleButton is pressed on or off 
+              title: Text(
+                "Gluten Free",
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: Theme.of(context).colorScheme.onBackground),
+              ),
+              subtitle: const Text("Only includes Gluten-Free Meals "),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+            )
+          ,  SwitchListTile(
+              value: _lactoseFreeFilterset,
+              onChanged: (ischecked) {setState(() {
+                _lactoseFreeFilterset=ischecked;
+              });}, 
+              title: Text(
+                "Lactose Free ",
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: Theme.of(context).colorScheme.onBackground),
+              ),
+              subtitle: const Text("Only includes Lactose-Free Meals "),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+            ),  SwitchListTile(
+              value: vegatarianFilterset,
+              onChanged: (ischecked) {setState(() {
+                vegatarianFilterset=ischecked;
+              });}, 
+              title: Text(
+                "Vegetarian",
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: Theme.of(context).colorScheme.onBackground),
+              ),
+              subtitle: const Text("Only includes Vegetarian Meals "),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+             ), SwitchListTile(
+              value: _veganFilterset,
+              onChanged: (ischecked) {setState(() {
+                _veganFilterset=ischecked;
+              });}, 
+              title: Text(
+                "Vegan",
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: Theme.of(context).colorScheme.onBackground),
+              ),
+              subtitle: const Text("Only includes Vegan  Meals "),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+            )],
+        ),
       ),
     );
   }
